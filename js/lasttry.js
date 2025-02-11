adef458902eb0fae414b98c0dda941031905b539
nask0123 <abubahan@gmail.com>
2025-02-12 00:54:27 +0500

    bycryptjs skachal

--- js/lasttry.js
+++ js/lasttry.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const session = require("express-session");  // Add this line
 
